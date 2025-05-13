646129905f7c148283c8a64b2f133bd2d7d6d4d2
Seetansh <87255041+Seetansh@users.noreply.github.com>
2025-05-13 19:27:35 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,16 @@
 function indexOfIgnoreCase(s1, s2) {
-  // write your code here
+  // write your code 
+  s1 = s1.toLowerCase();
+  s2 = s2.toLowerCase();
+	for(let i=0; i<s1.length; i++){
+	    if(s2[0] == s1[i]){
+	        return i;
+	    }
+	    else{
+	        continue;
+	    }
+	}
+	return -1;
 }
 
 // Please do not change the code below
